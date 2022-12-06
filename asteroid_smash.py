d33e639bf89df3f3d5ff370c373c4c02c359384a
Khrapenkov <stephan_khrapenkov@mail.ru>
2022-12-06 16:45:31 +0300

    class asteroid

--- asteroid_smash.py
+++ asteroid_smash.py
@@ -27,97 +27,93 @@ CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
-class SpaceShip(self):
+'''class SpaceShip(self):
     def __init__(self):
 class Bullet(self):
-sdjhg
+    sdjhg'''
 
 
-class Asteroid(self):
-    def __init__(self, rank):
-def create_circles():
-    '''создаёт и рисует произвольные n кружков '''
-    global x, y, r, color, vx, vy
-    x = []
-    y = []
-    r = []
-    vx = []
-    vy = []
-    color = []
-    for i in range(n):
-        x += [randint(Rmax, WIDTH-Rmax)]
-        y += [randint(Rmax, HEIGHT - Rmax)]
-        r += [randint(Rmin, Rmax)]
-        vx += [randint(Vmin, Vmax)*(2*randint(0, 1)-1)]
-        vy += [randint(Vmin, Vmax)*(2*randint(0, 1)-1)]
-        color += [COLORS[randint(0, 5)]]
-        circle(screen, color[i], (x[i], y[i]), r[i])
+class Asteroid:
+    def __init__(self, screen: pygame.Surface):
+        self.screen = screen
+        self.x = []
+        self.y = []
+        self.r = []
+        self.vx = []
+        self.vy = []
+        self.color = []
+    def create(self):
+        for i in range(n):
+            self.x += [randint(Rmax, WIDTH-Rmax)]
+            self.y += [randint(Rmax, HEIGHT - Rmax)]
+            self.r += [randint(Rmin, Rmax)]
+            self.vx += [randint(Vmin, Vmax)*(2*randint(0, 1)-1)]
+            self.vy += [randint(Vmin, Vmax)*(2*randint(0, 1)-1)]
+            self.color += [COLORS[randint(0, 5)]]
+            circle(screen, self.color[i], (self.x[i], self.y[i]), self.r[i])
 
-def catch_check(event):
-    '''проверяет, попал ли игрок в кружок, и удаляет данные о кружке, в который попали'''
-    global x, y, r
-    for i in range(n):
-        if (event.pos[0]-x[i])**2 + (event.pos[1]-y[i])**2 <= r[i]**2:
-            x.remove(x[i])
-            y.remove(y[i])
-            r.remove(r[i])
-            vx.remove(vx[i])
-            vy.remove(vy[i])
-            color.remove(color[i])
-            return True
-    return False
+    def catch_check(self, event):
+        '''п'''
+        for i in range(n):
+            if (event.pos[0]-self.x[i])**2 + (event.pos[1]-self.y[i])**2 <= self.r[i]**2:
+                self.x.remove(self.x[i])
+                self.y.remove(self.y[i])
+                self.r.remove(self.r[i])
+                self.vx.remove(self.vx[i])
+                self.vy.remove(self.vy[i])
+                self.color.remove(self.self.color[i])
+                return True
+        return False
 
-def new_circle():
-    '''создаёт и рисует новый кружок: заносит данные о нём в список кружков; т.е. вместе с
-    функцией "catch_check(event)" заменяет пойманный кружок на новый'''
-    global x, y, r, color, vx, vy
-    x += [randint(Rmax, WIDTH - Rmax)]
-    y += [randint(Rmax, HEIGHT - Rmax)]
-    r += [randint(Rmin, Rmax)]
-    vx += [randint(Vmin, Vmax) * (2 * randint(0, 1) - 1)]
-    vy += [randint(Vmin, Vmax) * (2 * randint(0, 1) - 1)]
-    color += [COLORS[randint(0, 5)]]
-    circle(screen, color[n-1], (x[n-1], y[n-1]), r[n-1])
+    def new(self):
+        '''с'''
+        self.x += [randint(Rmax, WIDTH - Rmax)]
+        self.y += [randint(Rmax, HEIGHT - Rmax)]
+        self.r += [randint(Rmin, Rmax)]
+        self.vx += [randint(Vmin, Vmax) * (2 * randint(0, 1) - 1)]
+        self.vy += [randint(Vmin, Vmax) * (2 * randint(0, 1) - 1)]
+        self.color += [COLORS[randint(0, 5)]]
+        circle(screen, self.color[n-1], (self.x[n-1], self.y[n-1]), self.r[n-1])
 
-def move_circles():
-    '''создаёт новые координаты центров кружков'''
-    for i in range(n):
-        x[i] += vx[i]
-        y[i] += vy[i]
+    def move(self):
+        '''с'''
+        for i in range(n):
+            self.x[i] += self.vx[i]
+            self.y[i] += self.vy[i]
 
-def wall_check():
-    '''проверяет необходимость отскока и отражает скорости кружков если нужно'''
-    for i in range(n):
-        if min(WIDTH-x[i], x[i]) <= r[i]:
-            vx[i] = -vx[i]
-        if min(HEIGHT - y[i], y[i]) <= r[i]:
-            vy[i] = -vy[i]
+    def wall_check(self):
+        '''проверяет необходимость отскока и отражает скорости астероидов если нужно'''
+        for i in range(n):
+            if min(WIDTH-self.x[i], self.x[i]) <= self.r[i]:
+                self.vx[i] = -self.vx[i]
+            if min(HEIGHT - self.y[i], self.y[i]) <= self.r[i]:
+                self.vy[i] = -self.vy[i]
 
-def draw_circles():
-    '''в отличие от функции "create_circules()" рисует уже заданные и перемещённые
-    функцией "move_circles()" кружки'''
-    for i in range(n):
-        circle(screen, color[i], (x[i], y[i]), r[i])
+    def draw(self):
+        '''в'''
+        for i in range(n):
+            circle(screen, self.color[i], (self.x[i], self.y[i]), self.r[i])
 
 score = 0
+asteroid = Asteroid(screen)
 shrift = pygame.font.SysFont('Times New Roman', 30)
 text_color = RED
 clock = pygame.time.Clock()
 finished = False
 
-create_circles()
+asteroid.create()
 while not finished:
     clock.tick(FPS)
     screen.blit(background, background_rect)
-    move_circles()
-    wall_check()
-    draw_circles()
+    asteroid.move()
+    asteroid.wall_check()
+    asteroid.draw()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            if catch_check(event):
-                new_circle()
+            if asteroid.catch_check(event):
+                asteroid.new()
                 score += 1
     pygame.display.update()
     screen.fill(BLACK)
