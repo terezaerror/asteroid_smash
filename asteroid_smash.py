638d8625ea17224b84bb8b266c7270383bb11aee
terezaerror <teradmnv@gmail.com>
2022-12-06 16:09:06 +0300

    backbone of the project

---
+++ asteroid_smash.py
@@ -0,0 +1,15 @@
+import pygame
+import math
+import random
+
+pygame.init()
+
+class SpaceShip(self):
+    def __init__(self):
+class Bullet(self):
+
+
+class Asteroid(self):
+    def __init__(self, rank):
+
+pygame.quit()
